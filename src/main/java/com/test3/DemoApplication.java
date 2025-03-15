fff5aac7d100a9e0ad5d7b038bf35c6d15dabd03
somu1999-web <beherasoumyajit231@gmail.com>
2025-03-15 19:40:25 +0530

    Update DemoApplication.java

--- src/main/java/com/test3/DemoApplication.java
+++ src/main/java/com/test3/DemoApplication.java
@@ -7,6 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		String name = "test";
+		int x = 100;
+		boolean isPresent = false;
+		char y = 'a';
+		float a = 10.3f;
+		boolean isSub = true;  // dev_1
+		var star = "dine";
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
